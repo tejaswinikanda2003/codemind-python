186da1b56ca620a375c0f5e46dd5d58c3064e8d0
Tejaswini <97425786+tejaswinikanda2003@users.noreply.github.com>
2022-05-19 19:30:42 +0530

    import math class Solution:    def solve(self, n):       if n < 10:          return n       s = 0       l = math.floor(math.log(n, 10) + 1)       while l > 0:          s += n % 10          n //= 10          l -= 1       return self.solve(s) ob = Solution() n=int(input()) print(ob.solve(n))

---
+++ Add_Digits.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+   def solve(self, n):
+      if n < 10:
+         return n
+      s = 0
+      l = math.floor(math.log(n, 10) + 1)
+      while l > 0:
+         s += n % 10
+         n //= 10
+         l -= 1
+      return self.solve(s)
+ob = Solution()
+n=int(input())
+print(ob.solve(n))
