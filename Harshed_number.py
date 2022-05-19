78ff2b716b0d0ee1cefe0ca6d4572341d0efa553
Tejaswini <97425786+tejaswinikanda2003@users.noreply.github.com>
2022-05-19 20:29:59 +0530

    num =int(input()) rem = sum = 0 #Make a copy of num and store it in variable n n = num
    #Calculates sum of digits    
    while(num > 0):    
        rem = num%10 
        sum = sum + rem    
        num = num//10   
         
    #Checks whether the number is divisible by the sum of digits    
    if(n%sum == 0):    
        print("True")
    else:    
        print("False")

---
+++ Harshed_number.py
@@ -0,0 +1,16 @@
+num =int(input())    
+rem = sum = 0
+#Make a copy of num and store it in variable n    
+n = num  
+     
+#Calculates sum of digits    
+while(num > 0):    
+    rem = num%10 
+    sum = sum + rem    
+    num = num//10   
+     
+#Checks whether the number is divisible by the sum of digits    
+if(n%sum == 0):    
+    print("True")
+else:    
+    print("False")
