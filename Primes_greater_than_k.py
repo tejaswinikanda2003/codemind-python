2febb3ce1090155193caaf57812dd786ffd99f10
Tejaswini <97425786+tejaswinikanda2003@users.noreply.github.com>
2022-06-29 21:39:41 +0530

    def pn(n):     c=0     for i in range(1,n+1):         if n%i==0:             c=c+1     if c==2:         return True     else:         return False def pg(x):     d=0     for k in x:         if pn(k):             if k>=m:                 d=d+1     print(d) n=int(input()) x=list(map(int,input().split())) m=int(input()) pg(x)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,20 @@
+def pn(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c=c+1
+    if c==2:
+        return True
+    else:
+        return False
+def pg(x):
+    d=0
+    for k in x:
+        if pn(k):
+            if k>=m:
+                d=d+1
+    print(d)
+n=int(input())
+x=list(map(int,input().split()))
+m=int(input())
+pg(x)
